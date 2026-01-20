481382a8bc47285564391caaa1e4bb8d00b9a1f1
Zach Bedewi <zachary.bedewi@protonmail.com>
2026-01-20 13:06:22 -0600

    Solution for 1673

---
+++ leetcode/1673-find-the-most-competitive-subsequence/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        vector<int> st;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            while (!st.empty() && st.back() > nums[i] && nums.size() - i - 1 >= k - st.size()) {
+                st.pop_back();
+            }
+            if (st.size() < k) {
+                st.push_back(nums[i]);
+            }
+        }
+        return st;
+    }
+};
