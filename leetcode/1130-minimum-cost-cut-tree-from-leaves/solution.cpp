4a0949eb5e813fce39d134999f1c28ed7396d954
Zach Bedewi <zachary.bedewi@protonmail.com>
2026-05-14 21:29:52 -0500

    Solution to 1130

---
+++ leetcode/1130-minimum-cost-cut-tree-from-leaves/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mctFromLeafValues(vector<int>& arr) {
+        int res = 0;
+        vector<int> stack{numeric_limits<int>::max()};
+
+        for (const int a : arr) {
+            while (stack.back() <= a) {
+                int mid = stack.back();
+                stack.pop_back();
+                res += mid * min(stack.back(), a);
+            }
+            stack.push_back(a);
+        }
+
+        for (int i = 2; i < stack.size(); ++i) {
+            res += stack[i] * stack[i - 1];
+        }
+
+        return res;
+    }
+};
